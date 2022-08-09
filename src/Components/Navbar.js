7ba2a95c6054e6dfa990bcb70c68370fcc2bdfab
drasti1999 <drastivaistra@gmail.comgit config --global user.email drastivaistra@gmail.comgit config --global user.email drastivaistra@gmail.com>
2022-08-09 16:38:48 +0530

    commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -34,7 +34,7 @@ export default function Navbar() {
               ></img>
             </a>
           </div>
-          <div className="col-lg-3 col-xl-4 col-md-5 col-sm-5 col-xs-12">
+          <div className="col-lg-3 col-xl-4 col-md-5 col-sm-5 col-xs-12 col-xs-visibility-hidden ">
           <ul class="d-flex g-0 m-0 p-0 justify-content-center text-white float-lg-end float-md-end  float-sm-end">
                 <div className="row">
                   <div className="col">
