a677db7d8f917e500b966309cc0a84bb5d498fd3
drasti1999 <drastivaistra@gmail.comgit config --global user.email drastivaistra@gmail.comgit config --global user.email drastivaistra@gmail.com>
2022-08-10 19:33:19 +0530

    margin fix till doctor experience commit

--- src/Components/Glance.js
+++ src/Components/Glance.js
@@ -3,24 +3,21 @@ import React from "react";
 export default function Glance() {
   return (
     <>
-      <div
-        class="section mt-5 mb-5"
-        style={{ backgroundColor: "#184E82" }}
-      >
+      <div class="section mt-5 mb-5" style={{ backgroundColor: "#184E82" }}>
         <div class="row w-100">
-          <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12 my-3"
-            id="doc"
-          >
+          <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12 my-3" id="doc">
             <div class="row">
               <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 center-block">
-                <h1 class="offset-lg-2 offset-md-3 h1 text-white">Meet Our Most</h1>
-                <h1 class="offset-lg-2  offset-md-3 h1 text-white">Experienced Doctors</h1>
+                <h1 class="offset-lg-2 offset-md-3 h1 text-white">
+                  Meet Our Most
+                </h1>
+                <h1 class="offset-lg-2  offset-md-3 h1 text-white">
+                  Experienced Doctors
+                </h1>
               </div>
             </div>
           </div>
-          <div
-            class="col-md-6 col-lg-6 col-sm-12 col-xs-12 d-lg-flex justify-content-center"
-          >
+          <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12 d-lg-flex justify-content-center">
             <img
               src="./images.jpg"
               alt=""
@@ -31,12 +28,13 @@ export default function Glance() {
         </div>
       </div>
       {/* At a glance */}
-
-      <div className="container mb-5">
-        <div className="row">
-          <h1 align="left" className="fw-bolder" style={{ color: "#184E82" }}>
-            At A Glance
-          </h1>
+      <div className="section pb-3 mx-4">
+        <div className="row mb-3 w-75 d-flex justify-content-lg-start justify-content-sm-center">
+          <div className="col-lg-12 col-md-12 col-sm-12 h1">
+            <h1 align="left" className="fw-bolder offset-lg-1 offset-md-2 offset-3" style={{ color: "#184E82",fontSize:"30px" }}>
+              At A Glance
+            </h1>
+          </div>
         </div>
         <div className="row text-center">
           <div className="col-lg-3 col-md-6 col-sm-12">
